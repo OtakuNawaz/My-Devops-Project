45fc3031aba616a4c59499a5202e6042eff81d56
OtakuNawaz <116078351+OtakuNawaz@users.noreply.github.com>
2022-12-23 08:37:48 +0530

    Update Calculator_test_file.py

--- Calculator_test_file.py
+++ Calculator_test_file.py
@@ -8,12 +8,12 @@ class TestCalc(unittest.TestCase):
         self.assertEqual(calculator.add(-4,-5),-9)
 
     def test_subtract(self):
-            self.assertEqual(calculator.subtract(10,5),4)
+            self.assertEqual(calculator.subtract(10,5),5)
             self.assertEqual(calculator.subtract(-1,5),-6)
             self.assertEqual(calculator.subtract(-4,-5),1)
 
     def test_multiply(self):
-            self.assertEqual(calculator.multiply(10,5),15)
+            self.assertEqual(calculator.multiply(10,5),50)
             self.assertEqual(calculator.multiply(-1,5),-5)
             self.assertEqual(calculator.multiply(-4,-5),20)
             self.assertEqual(calculator.multiply(1,-5),-5)
